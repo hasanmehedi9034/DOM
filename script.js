007e5a077f5c6ddb4753177be7c17aae4e74b5ab
hasanmehedi9034 <mehedi90344@gmail.com>
2023-02-02 15:32:03 +0600

    what is dom, object, method

---
+++ script.js
@@ -0,0 +1,10 @@
+// document.getElementById('demo').innerHTML = 'Hello Mehedi'
+
+// console.log(document.getElementById('demo'));
+console.dir(document.getElementById('demo'));
+
+const p = document.getElementById('demo');
+p.innerHTML = 'Hello World'
+
+
+
