318e6d96c1dd507c92e1206a3ac3dfecd01806d8
hasanmehedi9034 <mehedi90344@gmail.com>
2023-02-02 15:45:51 +0600

    get html object with id, classname, tag name

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 // document.getElementById('demo').innerHTML = 'Hello Mehedi'
 
 // console.log(document.getElementById('demo'));
-console.dir(document.getElementById('demo'));
+// console.dir(document.getElementById('demo'));
 
-const p = document.getElementById('demo');
-p.innerHTML = 'Hello World'
+// const p = document.getElementById('demo');
+// p.innerHTML = 'Hello World'
+
+// console.log(document.getElementsByTagName('p'));
+
+// console.log(document.getElementsByClassName('anchor'))
+
+// document.querySelector('.anchor').innerHTML = 'Click me'
+
+// console.log(document.querySelectorAll('p'))
+
+const a = document.getElementById('anchor');
+console.log(a.innerHTML)
 
 
 
